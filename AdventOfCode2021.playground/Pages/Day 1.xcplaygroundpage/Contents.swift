4d2ef2a3f243909cbb01063159b624ae26f97fa6
jcollins1983 <jason.collins83@gmail.com>
2021-12-01 21:59:21 +1100

    added some comments

--- AdventOfCode2021.playground/Pages/Day 1.xcplaygroundpage/Contents.swift
+++ AdventOfCode2021.playground/Pages/Day 1.xcplaygroundpage/Contents.swift
@@ -4,16 +4,18 @@ import Cocoa
 
 let file = "P1.txt"
 
-guard let fileUrl = Bundle.main.url(forResource: "P1", withExtension: ".txt") else { fatalError()}
+// get file handle
+guard let fileUrl = Bundle.main.url(forResource: "P1", withExtension: ".txt") else { fatalError() }
+// read the file
 let text = try String(contentsOf: fileUrl, encoding: String.Encoding.utf8)
-
+// split the file into individual elements
 let vals = text.split(separator: "\n")
 // map to Int
 var p1_list:[Int] = vals.map {Int($0)!}
 
 var increased = 0
 
-// find total increased
+// find total increased starting from second element
 for idx in 1...p1_list.count - 1
 {
     if p1_list[idx] > p1_list[idx - 1]
@@ -24,9 +26,10 @@ for idx in 1...p1_list.count - 1
 print(increased)
 
 // find increased from moving window of three
-increased = 0
+increased = 0 // reusing increased because lazy
 for idx in 0...p1_list.count - 1
 {
+    // I had two options, only iterate up to the end of the array - 4 for the last set or use this guard statement.
     guard idx <= p1_list.count - 4 else {
         break // we don't have enough to sum 3
     }
