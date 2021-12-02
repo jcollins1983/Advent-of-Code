c4d3bcdc2630825104017ed23fa70ca71f640190
jcollins1983 <jason.collins83@gmail.com>
2021-12-02 21:01:41 +1100

    day 2 added.

--- AdventOfCode2021.playground/Pages/Day 2.xcplaygroundpage/Contents.swift
+++ AdventOfCode2021.playground/Pages/Day 2.xcplaygroundpage/Contents.swift
@@ -2,6 +2,53 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+let file = "P2.txt"
 
+// get file handle
+guard let fileUrl = Bundle.main.url(forResource: "P2", withExtension: ".txt") else { fatalError() }
+// read the file
+let lines = try String(contentsOf: fileUrl, encoding: String.Encoding.utf8).split(separator: "\n")
+
+// part 1
+var total_horizontal = 0
+var total_depth = 0
+
+for line in lines {
+    let cmd = line.split(separator: " ")
+    switch cmd[0]
+    {
+        case "forward":
+            total_horizontal += Int(cmd[1])!
+        case "up":
+            total_depth -= Int(cmd[1])!
+        case "down":
+            total_depth += Int(cmd[1])!
+        default:
+            break
+    }
+}
+
+print("Total forward: \(total_horizontal)\tTotal depth: \(total_depth)\tProduct: \(total_horizontal * total_depth)")
+
+// part 2
+total_horizontal = 0
+total_depth = 0
+var aim = 0
+
+for line in lines {
+    let cmd = line.split(separator: " ")
+    switch cmd[0]
+    {
+        case "forward":
+            total_horizontal += Int(cmd[1])!
+            total_depth += aim * Int(cmd[1])!
+        case "up":
+            aim -= Int(cmd[1])!
+        case "down":
+            aim += Int(cmd[1])!
+        default:
+            break
+    }
+}
+print("Total Horizontal: \(total_horizontal)\tTotal Depth: \(total_depth)\t Product: \(total_depth * total_horizontal)")
 //: [Next](@next)
