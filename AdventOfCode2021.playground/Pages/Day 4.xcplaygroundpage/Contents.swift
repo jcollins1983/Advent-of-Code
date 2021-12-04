3194b420bc4e11ad7f1b3f917faf929556941333
jcollins1983 <jason.collins83@gmail.com>
2021-12-05 01:15:18 +1100

    getting there. shouldn't be too much work to get part 1 done from here.

--- AdventOfCode2021.playground/Pages/Day 4.xcplaygroundpage/Contents.swift
+++ AdventOfCode2021.playground/Pages/Day 4.xcplaygroundpage/Contents.swift
@@ -13,32 +13,78 @@ let num_seq = data[0].split(separator: ",").map{Int($0)!}
 // get the board data from rest of the file, the below results in each element being an array containing the 5 rows.
 let boardData = data[1...].split(separator: "")
 
-print(boardData[0])
 struct BingoBoard
 {
-    let rows = 5
-    let cols = 5
-    var board = [[Int]]()//(repeating: [0, 0, 0, 0, 0], count: 5)
+    let numRows = 5
+    let numCols = 5
+    var board = [[Int]]()
+    private var matched = [[Bool]](repeating: [Bool](repeating: false, count: 5), count: 5)
+    private var matchRowCounts:[Int:Int] = [0: 0, 1: 0, 2: 0, 3: 0, 4: 0]
+    private var matchColCounts:[Int:Int] = [0: 0, 1: 0, 2: 0, 3: 0, 4: 0]
+    var winner: Bool = false
     
-    init(boardVal:[[ArraySlice<String>]])
+    init(boardVal:ArraySlice<String>)
     {
         // a list of strings is passed in, this is then assigned as necessary to each element.
-        for board_row in boardVal
+        for vals in boardVal
         {
-            print(board_row)
+            // read the line into board array, split by whitespace and filter out the empty strings from the single digit numbers
+            board.append(vals.components(separatedBy: .whitespaces).filter{$0 != ""}.map{Int($0)!})
         }
         
     }
     
-    func check_num(num:Int)
+    mutating func checkNum(num:Int)
     {
-        for itm in board
+        // check to see if the number is on this board.
+        for row in 0..<numRows
         {
-            print(itm)
+            for col in 0..<numCols
+            {
+                if num == board[row][col]
+                {
+                    matched[row][col] = true
+                    matchColCounts[col]! += 1
+                    matchRowCounts[row]! += 1
+                }
+            }
         }
+    }
+    
+    private mutating func checkMatchedCounts()
+    {
+        for key in 0..<numRows
+        {
+            if matchRowCounts[key] == numRows
+            {
+                winner = true
+            }
+        }
+        for key in 0..<numCols
+        {
+            if matchColCounts[key] == numCols
+            {
+                winner = true
+            }
+        }
+    }
+    
+    mutating func isWinner() -> Bool
+    {
+        // check the match counts
+        checkMatchedCounts()
+        return winner
     }
 }
 
-
+// create the boards
 var board = BingoBoard(boardVal: boardData[0])
+
+// check the numbers in the boards. Stop on winner. Get sum.
+board.checkNum(num: 5)
+board.checkNum(num: 31)
+board.checkNum(num: 70)
+board.checkNum(num: 8)
+board.checkNum(num: 88)
+board.isWinner()
 //: [Next](@next)
