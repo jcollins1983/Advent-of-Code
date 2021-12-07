35124a57d8ea2977a3073e1933f73b66496a17ae
jcollins1983 <jason.collins83@gmail.com>
2021-12-07 21:08:23 +1100

    Day 5 part 1 working

--- AdventOfCode2021.playground/Pages/Day 5.xcplaygroundpage/Contents.swift
+++ AdventOfCode2021.playground/Pages/Day 5.xcplaygroundpage/Contents.swift
@@ -7,37 +7,54 @@ guard let fileUrl = Bundle.main.url(forResource: "P5", withExtension: ".txt") el
 let data = try String(contentsOf: fileUrl, encoding: String.Encoding.utf8).components(separatedBy: .newlines)
 
 // read the lines from data, split on separator -> then split on , in each pair and map to Int followed by assignment to NSPoint in named tuple with p1 and p2 names. There's probably a cleaner way to do this, but it works.
-let lines = data.map{ (p1:NSPoint(x:$0.components(separatedBy: " -> ")[0].split(separator: ",").map{Int($0)!}[0],
-                              y:$0.components(separatedBy: " -> ")[0].split(separator: ",").map{Int($0)!}[1]),
-                       p2:NSPoint(x:$0.components(separatedBy: " -> ")[1].split(separator: ",").map{Int($0)!}[0],
-                               y:$0.components(separatedBy: " -> ")[1].split(separator: ",").map{Int($0)!}[1]))}
+//let lines = data.map{ (p1:NSPoint(x:$0.components(separatedBy: " -> ")[0].split(separator: ",").map{Int($0)!}[0],
+//                              y:$0.components(separatedBy: " -> ")[0].split(separator: ",").map{Int($0)!}[1]),
+//                       p2:NSPoint(x:$0.components(separatedBy: " -> ")[1].split(separator: ",").map{Int($0)!}[0],
+//                               y:$0.components(separatedBy: " -> ")[1].split(separator: ",").map{Int($0)!}[1]))}
+// split the data into point pairs
+let linesRaw = data.map { $0.components(separatedBy: " -> ") }
+// set up list of named tuples for the points and cast to int.
+let lines = linesRaw.map { (x1: Int($0[0].split(separator: ",")[0]) ?? 0,
+                            y1: Int($0[0].split(separator: ",")[1]) ?? 0,
+                            x2: Int($0[1].split(separator: ",")[0]) ?? 0,
+                            y2: Int($0[1].split(separator: ",")[1]) ?? 0) }
 
-// there has to be a better way to do this, but I'm still learning how these closures work, or maybe my chosen data structure was a poor choice. 🤷‍♂️
-// get the max x & y values to be used on the grid
-// the first bit here maps the largest x value from each set of points into one array, then call max on that array to get the largest x
-// also need to cast from CGFloat to Int
-let maxX = Int(lines.map { max($0.p1.x, $0.p1.x, $0.p2.x, $0.p2.x) }.max() ?? 0)
-let maxY = Int(lines.map { max($0.p1.y, $0.p1.y, $0.p2.y, $0.p2.y) }.max() ?? 0)
+// get max x and y values for creating the grid to keep track of line intersections/overlaps.
+let maxX  = lines.map { max($0.x1, $0.x2)}.max() ?? 1
+let maxY  = lines.map { max($0.y1, $0.y2)}.max() ?? 1
+// create empty grid maxX x maxY
+print("Grid needs to be \(maxX) x \(maxY)")
+var grid = [[Int]](repeating: [Int](repeating: 0, count: maxX + 2), count: maxY + 2)
 
-var grid = [[Int]](repeating: [Int](repeating: 0, count: maxX), count: maxY)
-
-// put lines on a grid, 1:1 mapping of array index to point (probs shouldn't be using swift keywords, but I read that if you do you should enclose them in back ticks.
+// put the lines on the grid
+// probably shouldn't be using swift keywords, but I read that if you do you should enclose it in back ticks
 for `line` in lines
 {
-    // outer loop
-    for xVal in stride(from: `line`.p1.x, to: `line`.p2.x, by: 1)
+    print("Looking at line \(`line`)")
+    // add one to the each point on the line
+    if `line`.x1 == `line`.x2
     {
-        // inner loop
-        for yVal in stride(from: `line`.p1.y, to: `line`.p2.y, by: 1)
+        // the line is vertical
+        // don't know which point will be higher
+        for py in min(`line`.y1, `line`.y2)...max(`line`.y1, `line`.y2)
         {
-            // add one to the point on the grid, need to cast to Int because the values take on the type of the strideable which in this case is CGFLoat because of the NSPoint object (as above, probably a poor choice in data structure.
-            grid[Int(xVal)][Int(yVal)] += 1
+            print("Adding point: x: \(`line`.x1) y: \(py)")
+            grid[`line`.x1][py] += 1
+        }
+    }
+    else if `line`.y1 == `line`.y2
+    {
+        // the line is horizontal
+        // don't know which point will be higher
+        for px in min(`line`.x1, `line`.x2)...max(`line`.x1, `line`.x2)
+        {
+            print("Adding point: x: \(px) y: \(`line`.y1)")
+            grid[px][`line`.y1] += 1
         }
     }
 }
-
-// get the elements where the number is 2 or more
+// count the points that were encountered more than once, flatten the grid first, then filter.
 let flatGrid = grid.flatMap { $0 }
 let countGreater2 = flatGrid.filter{ $0 >= 2 }
 print("Number of points >2: \(countGreater2.count)")
-//: [Next](@next)
+////: [Next](@next)
