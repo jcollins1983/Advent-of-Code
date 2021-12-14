248684be394e897659aa1c7021d0d0f2d7dc0639
jcollins1983 <jason.collins83@gmail.com>
2021-12-14 21:35:54 +1100

    Uncommented part 1

--- AdventOfCode2021.playground/Pages/Day 7.xcplaygroundpage/Contents.swift
+++ AdventOfCode2021.playground/Pages/Day 7.xcplaygroundpage/Contents.swift
@@ -8,34 +8,34 @@ let crabPositions = try String(contentsOf: fileUrl, encoding: String.Encoding.ut
 
 // Part 1
 // brute force
-//var fuelBurnBrute = [Int]()
-//for pos in crabPositions
-//{
-//    fuelBurnBrute.append(crabPositions.map { abs($0 - pos)}.reduce(0, +))
-//
-//}
-//print("Brute Force Method: \(fuelBurnBrute.min()!)")
-//
-//// using median https://math.stackexchange.com/questions/113270/the-median-minimizes-the-sum-of-absolute-deviations-the-ell-1-norm
-//func getMedian(array: [Int]) -> Float
-//{
-//    // sort the array
-//    let sorted = array.sorted()
-//    let numElements = sorted.count
-//    if numElements % 2 != 0
-//    {
-//        // median is the last index / 2
-//        return Float(sorted[((numElements - 1) / 2)])
-//    }
-//    else
-//    {
-//        // median is the average of the 2 values in the middle
-//        return Float((sorted[(numElements / 2) - 1] + sorted[numElements / 2]) / 2)
-//    }
-//}
-//let median = getMedian(array: crabPositions)
-//let fuelBurnMedian = crabPositions.map { abs(Float($0) - median) }.reduce(0, +)
-//print("Median method: \(fuelBurnMedian)")
+var fuelBurnBrute = [Int]()
+for pos in crabPositions
+{
+    fuelBurnBrute.append(crabPositions.map { abs($0 - pos)}.reduce(0, +))
+
+}
+print("Brute Force Method: \(fuelBurnBrute.min()!)")
+
+// using median https://math.stackexchange.com/questions/113270/the-median-minimizes-the-sum-of-absolute-deviations-the-ell-1-norm
+func getMedian(array: [Int]) -> Float
+{
+    // sort the array
+    let sorted = array.sorted()
+    let numElements = sorted.count
+    if numElements % 2 != 0
+    {
+        // median is the last index / 2
+        return Float(sorted[((numElements - 1) / 2)])
+    }
+    else
+    {
+        // median is the average of the 2 values in the middle
+        return Float((sorted[(numElements / 2) - 1] + sorted[numElements / 2]) / 2)
+    }
+}
+let median = getMedian(array: crabPositions)
+let fuelBurnMedian = crabPositions.map { abs(Float($0) - median) }.reduce(0, +)
+print("Median method: \(fuelBurnMedian)")
 
 // Part 2
 // nth triangle number https://math.stackexchange.com/questions/593318/factorial-but-with-addition/593323 and https://i1115.photobucket.com/albums/k544/akinuri/nth%20triangle%20number-01.jpg
