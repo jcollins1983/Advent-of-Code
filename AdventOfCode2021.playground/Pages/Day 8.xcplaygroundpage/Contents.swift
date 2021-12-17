cfb6d37b5429622c3c17b8886d6b93504b55b3a6
jcollins1983 <jason.collins83@gmail.com>
2021-12-17 15:34:21 +1100

    Day 8 Part 2 working (but a hacky mess)

--- AdventOfCode2021.playground/Pages/Day 8.xcplaygroundpage/Contents.swift
+++ AdventOfCode2021.playground/Pages/Day 8.xcplaygroundpage/Contents.swift
@@ -32,5 +32,105 @@ for idx in 0..<displayData.count
     uniquesCount += countUniques(dispOutput: displayData[idx].dispOutput)
 }
 
-print(uniquesCount)
+print("Part 1: \(uniquesCount)")
+
+// part 2
+func decodeDisplay(wirePattern str: String, displayData disp:String) -> Int
+{
+    var decodeMap = [String:Int]()
+    var tempDict = [Int:String]()
+    let patternSubStrings = str.split(separator: " ")
+    var length5SubStrings = [String]()
+    var length6SubStrings = [String]()
+    for subString in patternSubStrings
+    {
+        // we already know which elements are 1, 4, 7 & 8
+        switch subString.count
+        {
+            case 2:
+                // we have a 1
+                tempDict[1] = String(subString)
+                decodeMap[String(subString.sorted())] = 1
+            case 3:
+                // we have a 7
+                tempDict[7] = String(subString)
+                decodeMap[String(subString.sorted())] = 7
+            case 4:
+                // we have a 4
+                tempDict[4] = String(subString)
+                decodeMap[String(subString.sorted())] = 4
+            case 7:
+                // we have an 8
+                tempDict[8] = String(subString)
+                decodeMap[String(subString.sorted())] = 8
+            case 5:
+                // it could be a 2, 3, or 5, we need to parse all strings before determining, so need to store for later.
+                length5SubStrings.append(String(subString))
+            case 6:
+                // it could be a 6, 9, or 0, we need to parse all strings before determining, so need to store for later.
+                length6SubStrings.append(String(subString))
+            default:
+                break
+        }
+    }
+    
+    // find the 6, 9 & 0
+    for subStr in length6SubStrings
+    {
+        if Set(subStr).union(Set(tempDict[4]!)) == Set(subStr)
+        {
+            // this is a 9
+            decodeMap[String(subStr.sorted())] = 9
+            // add to tempDict for use in working out the length 5 codes.
+            tempDict[9] = subStr
+        }
+        else if Set(subStr).union(Set(tempDict[1]!)) == Set(subStr)
+        {
+            // it's a 0
+            decodeMap[String(subStr.sorted())] = 0
+            // add to tempDict for use in working out the length 5 codes.
+            tempDict[0] = subStr
+        }
+        else
+        {
+            // it's a 6
+            decodeMap[String(subStr.sorted())] = 6
+            // add to tempDict for use in working out the length 5 codes.
+            tempDict[6] = subStr
+        }
+    }
+    
+    // find the 2, 3 & 5
+    for subStr in length5SubStrings
+    {
+        if Set(subStr).union(tempDict[1]!) == Set(subStr)
+        {
+            // we have a 3
+            decodeMap[String(subStr.sorted())] = 3
+        }
+        // create union with 1, if it looks like a 9, it's a 5
+        else if Set(subStr).union(tempDict[9]!) == Set(subStr).union(tempDict[1]!)
+        {
+            // we have a 5
+            decodeMap[String(subStr.sorted())] = 5
+        }
+        else
+        {
+            // we have a 2
+            decodeMap[String(subStr.sorted())] = 2
+        }
+    }
+    
+    // work out the displa value
+    let dispSubStrings = disp.split(separator: " ")
+    var val = 0
+    val += decodeMap[String(dispSubStrings[0].sorted())]! * 1000
+    val += decodeMap[String(dispSubStrings[1].sorted())]! * 100
+    val += decodeMap[String(dispSubStrings[2].sorted())]! * 10
+    val += decodeMap[String(dispSubStrings[3].sorted())]!
+    return val
+}
+
+let dispValSum = displayData.map{ decodeDisplay(wirePattern: $0.wirePattern, displayData: $0.dispOutput) }.reduce(0, +)
+print("Part 2: \(dispValSum)")
 //: [Next](@next)
